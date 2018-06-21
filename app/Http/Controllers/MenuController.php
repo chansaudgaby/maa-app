6c15ce8f8cea82da69b6d2d34eddc4650250afe0
chansaudgaby <chansaud.eremoana@gmail.com>
2018-06-20 16:19:40 -1000

    introduction de la jointure controller

--- app/Http/Controllers/MenuController.php
+++ app/Http/Controllers/MenuController.php
@@ -1,15 +1,15 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Illuminate\Support\Facades\Auth;
 use App\Menu;
 use App\User;
 use App\Meal;
+use App\UserType;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Requests;
 use Illuminate\Support\Facades\Response;
-use Illuminate\Support\Facades\Auth; 
 use Illuminate\Support\Facades\DB;
 use App\Http\Resources\Menu as MenuR;
 
@@ -17,23 +17,38 @@ class MenuController extends Controller
 {
     public function all()
     {
-        $menus = Menu::select('id','user_id','meal_id','date')->paginate(25);
-        
-        foreach ($menus as $key=>$menu) {
-            $user = User::where('id','=',$menu->user_id)->select('fname as FirstName', 'lname as LastName')->get();
-            //dd($user);
-            $menus[$key]->user = $user;   
-        }
-        foreach ($menus as $key=>$menu) {
-            $meal = Meal::where('id','=',$menu->meal_id)->select('name','user_id','picture')->get();
-            $menus[$key]->meal = $meal;
-        }
+        $userTypeId = Auth::user()->userstype_id;
+        //====Si l'utilisateur est un traiteur alors
+        if($userTypeId == 1):
+            $menus = Menu::select('id','user_id','meal_id','date')->paginate(25);
+            foreach ($menus as $key=>$menu):
+                $user = User::where('id','=',$menu->user_id)->select('fname as FirstName', 'lname as LastName')->get();
+                $menus[$key]->user = $user; 
+            endforeach;
+            foreach ($menus as $key=>$menu):
+                $meal = Meal::where('id','=',$menu->meal_id)->select('name','user_id','picture')->get();
+                    $menus[$key]->meal = $meal;
+            endforeach;
         return Response::json($menus);
+        else:
+            return Response::json(['error'=>'accès non autorisé']);
+        endif;
     }
-
+    
     public function show($menuId) 
     {
-        $menu = Menu::findOrFail($menuId);
+        $userId = Auth::user()->id;
+        $menus = Menu::select('id','user_id','meal_id','date')
+        ->where('menus.id', '=', $menuId)
+        ->paginate(25);
+        foreach ($menus as $key=>$menu):
+            $user = User::where('id','=',$menu->user_id)->select('fname as FirstName', 'lname as LastName')->get();
+            $menus[$key]->user = $user; 
+        endforeach;
+        foreach ($menus as $key=>$menu):
+            $meal = Meal::where('id','=',$menu->meal_id)->select('name','user_id','picture')->get();
+                $menus[$key]->meal = $meal;
+        endforeach;
         return Response::json($menu);
     }
     
