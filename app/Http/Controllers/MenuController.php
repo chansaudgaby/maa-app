951e4f1d65bf751c73cd148232409bac2e25c1d5
chansaudgaby <chansaud.eremoana@gmail.com>
2018-06-20 15:14:32 -1000

    ajout date BDD menu et methode all ok

--- app/Http/Controllers/MenuController.php
+++ app/Http/Controllers/MenuController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Menu;
+use App\User;
+use App\Meal;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Requests;
@@ -14,7 +16,17 @@ class MenuController extends Controller
 {
     public function all()
     {
-        $menus = Menu::select('id','user_id','meal_id')->paginate(25);
+        $menus = Menu::select('id','user_id','meal_id','date')->paginate(25);
+        
+        foreach ($menus as $key=>$menu) {
+            $user = User::where('id','=',$menu->user_id)->select('fname as FirstName', 'lname as LastName')->get();
+            //dd($user);
+            $menus[$key]->user = $user;   
+        }
+        foreach ($menus as $key=>$menu) {
+            $meal = Meal::where('id','=',$menu->meal_id)->select('name','user_id','picture')->get();
+            $menus[$key]->meal = $meal;
+        }
         return Response::json($menus);
     }
 
